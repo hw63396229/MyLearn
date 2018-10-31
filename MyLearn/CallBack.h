f74c0e0f858341e8c1cbbb49f1be9c8bf6a28036
hw63396229 <hw63396229unity@126.com>
2018-10-31 17:10:41 +0800

    add

--- MyLearn/CallBack.h
+++ MyLearn/CallBack.h
@@ -3,12 +3,14 @@
 #include "Camera.h"
 #include <GLFW/glfw3.h>
 #include "stb_image.h"
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 unsigned int loadTexture(const char *path);
+unsigned int loadCubemap(const std::vector<std::string> &faces);
 
 // settings
 extern const unsigned int SCR_WIDTH;
--- MyLearn/LNframebuffers.cpp
+++ MyLearn/LNframebuffers.cpp
@@ -4,17 +4,14 @@
 //
 //int main()
 //{
-//	// glfw: initialize and configure
 //	glfwInit();
 //	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 //	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 //	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//    // glfw window creation
-//	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+//	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LNGL", NULL, NULL);
 //	if (window == NULL)
 //	{
-//		std::cout << "Failed to create GLFW window" << std::endl;
+//		std::cout << "glfw create window failed!" << std::endl;
 //		glfwTerminate();
 //		return -1;
 //	}
@@ -22,27 +19,20 @@
 //	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 //	glfwSetCursorPosCallback(window, mouse_callback);
 //	glfwSetScrollCallback(window, scroll_callback);
+//	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 //
-//	// tell GLFW to capture our mouse
-//	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-//
-//	// glad: load all OpenGL function pointers
 //	if (!gladLoadGL())
 //	{
-//		//!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)
-//		std::cout << "Failed to initialize GLAD" << std::endl;
+//		std::cout << "failed to initialize glad" << std::endl;
+//		glfwTerminate();
 //		return -1;
 //	}
 //
-//	// configure global opengl state
 //	glEnable(GL_DEPTH_TEST);
-//	glEnable(GL_BLEND);
-//	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//	
+//	Shader shader("5.1.framebuffers.vert", "5.1.framebuffers.frag");
+//	Shader screenShader("5.1.framebuffers_screen.vert", "5.1.framebuffers_screen.frag");
 //
-//	// build and compile shaders
-//	Shader shader("Blendingsort.vert", "Blendingsort.frag");
-//
-//	// set up vertex data (and buffer(s)) and configure vertex attributes
 //	float cubeVertices[] = {
 //		// positions          // texture Coords
 //		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -97,18 +87,19 @@
 //		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
 //		5.0f, -0.5f, -5.0f,  2.0f, 2.0f
 //	};
-//	float transparentVertices[] = {
-//		// positions         // texture Coords (swapped y coordinates because texture is flipped upside down)
-//		0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
-//		0.0f, -0.5f,  0.0f,  0.0f,  1.0f,
-//		1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+//	float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+//							 // positions   // texCoords
+//		-1.0f,  1.0f,  0.0f, 1.0f,
+//		-1.0f, -1.0f,  0.0f, 0.0f,
+//		1.0f, -1.0f,  1.0f, 0.0f,
 //
-//		0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
-//		1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
-//		1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+//		-1.0f,  1.0f,  0.0f, 1.0f,
+//		1.0f, -1.0f,  1.0f, 0.0f,
+//		1.0f,  1.0f,  1.0f, 1.0f
 //	};
-//	// cube VAO
-//	unsigned int cubeVAO, cubeVBO;
+//
+//	//cube VAO
+//	unsigned cubeVAO, cubeVBO;
 //	glGenVertexArrays(1, &cubeVAO);
 //	glGenBuffers(1, &cubeVBO);
 //	glBindVertexArray(cubeVAO);
@@ -129,68 +120,77 @@
 //	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 //	glEnableVertexAttribArray(1);
 //	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-//	// transparent VAO
-//	unsigned int transparentVAO, transparentVBO;
-//	glGenVertexArrays(1, &transparentVAO);
-//	glGenBuffers(1, &transparentVBO);
-//	glBindVertexArray(transparentVAO);
-//	glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
-//	glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+//	// screen quad VAO
+//	unsigned int quadVAO, quadVBO;
+//	glGenVertexArrays(1, &quadVAO);
+//	glGenBuffers(1, &quadVBO);
+//	glBindVertexArray(quadVAO);
+//	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
 //	glEnableVertexAttribArray(0);
-//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+//	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
 //	glEnableVertexAttribArray(1);
-//	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-//	glBindVertexArray(0);
+//	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 //
-//	// load textures
-//	unsigned int cubeTexture = loadTexture("../texture/marble.jpg");
-//	unsigned int floorTexture = loadTexture("../texture/metal.png");
-//	unsigned int transparentTexture = loadTexture("../texture/window.png");
+//	unsigned cubeTexture  = loadTexture("../texture/container.jpg");
+//	unsigned planeTexture = loadTexture("../texture/metal.png");
 //
-//	// transparent window locations
-//	vector<glm::vec3> windows
-//	{
-//		glm::vec3(-1.5f, 0.0f, -0.48f),
-//		glm::vec3(1.5f, 0.0f, 0.51f),
-//		glm::vec3(0.0f, 0.0f, 0.7f),
-//		glm::vec3(-0.3f, 0.0f, -2.3f),
-//		glm::vec3(0.5f, 0.0f, -0.6f)
-//	};
-//
-//	// shader configuration
 //	shader.use();
 //	shader.setInt("texture1", 0);
+//	screenShader.use();
+//	screenShader.setInt("screenTexture", 0);
 //
-//	// render loop
+//	// framebuffer configuration
+//	unsigned framebuffer;
+//	glGenFramebuffers(1, &framebuffer);
+//	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+//	// create a color attachment texture
+//	unsigned textureColorBuffer;
+//	glGenTextures(1, &textureColorBuffer);
+//	glBindTexture(GL_TEXTURE_2D, textureColorBuffer);
+//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorBuffer, 0);
+//	// create a renderbuffer object for depth and stencil attachment(we won't be sampling these)
+//	unsigned rbo;
+//	glGenRenderbuffers(1, &rbo);
+//	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+//	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+//	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+//																								  // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+//	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+//		cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+//	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+//	
+//	// draw as wireframe
+//	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//
 //	while (!glfwWindowShouldClose(window))
 //	{
 //		// per-frame time logic
-//		float currentFrame = (float)(glfwGetTime());
+//		float currentFrame = (float)glfwGetTime();
 //		deltaTime = currentFrame - lastFrame;
 //		lastFrame = currentFrame;
 //
 //		// input
 //		processInput(window);
 //
-//		// sort the transparent windows before rendering
-//		std::map<float, glm::vec3> sorted;
-//		for (unsigned int i = 0; i < windows.size(); i++)
-//		{
-//			float distance = glm::length(camera.Position - windows[i]);
-//			sorted[distance] = windows[i];
-//		}
-//
 //		// render
+//		// bind to framebuffer and draw scene as we normally would to color texture 
+//		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+//		glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+//
+//								 // make sure we clear the framebuffer's content
 //		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 //		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 //
-//		// draw objects
 //		shader.use();
-//		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-//		glm::mat4 view = camera.GetViewMatrix();
 //		glm::mat4 model;
-//		shader.setMat4("projection", projection);
+//		glm::mat4 view = camera.GetViewMatrix();
+//		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 //		shader.setMat4("view", view);
+//		shader.setMat4("projection", projection);
 //		// cubes
 //		glBindVertexArray(cubeVAO);
 //		glActiveTexture(GL_TEXTURE0);
@@ -204,32 +204,34 @@
 //		glDrawArrays(GL_TRIANGLES, 0, 36);
 //		// floor
 //		glBindVertexArray(planeVAO);
-//		glBindTexture(GL_TEXTURE_2D, floorTexture);
-//		model = glm::mat4();
-//		shader.setMat4("model", model);
+//		glBindTexture(GL_TEXTURE_2D, planeTexture);
+//		shader.setMat4("model", glm::mat4());
 //		glDrawArrays(GL_TRIANGLES, 0, 6);
-//		// windows (from furthest to nearest)
-//		glBindVertexArray(transparentVAO);
-//		glBindTexture(GL_TEXTURE_2D, transparentTexture);
-//		for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
-//		{
-//			model = glm::mat4();
-//			model = glm::translate(model, it->second);
-//			shader.setMat4("model", model);
-//			glDrawArrays(GL_TRIANGLES, 0, 6);
-//		}
+//		glBindVertexArray(0);
 //
+//		// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
+//		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+//		glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+//								  // clear all relevant buffers
+//		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessery actually, since we won't be able to see behind the quad anyways)
+//		glClear(GL_COLOR_BUFFER_BIT);
+//
+//		screenShader.use();
+//		glBindVertexArray(quadVAO);
+//		glBindTexture(GL_TEXTURE_2D, textureColorBuffer);	// use the color attachment texture as the texture of the quad plane
+//		glDrawArrays(GL_TRIANGLES, 0, 6);
 //
 //		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 //		glfwSwapBuffers(window);
 //		glfwPollEvents();
 //	}
 //
-//	// optional: de-allocate all resources once they've outlived their purpose:
 //	glDeleteVertexArrays(1, &cubeVAO);
 //	glDeleteVertexArrays(1, &planeVAO);
+//	glDeleteVertexArrays(1, &quadVAO);
 //	glDeleteBuffers(1, &cubeVBO);
 //	glDeleteBuffers(1, &planeVBO);
+//	glDeleteBuffers(1, &quadVBO);
 //
 //	glfwTerminate();
 //	return 0;
